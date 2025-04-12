595a5d71fc60bc095e53abe9768389c620a2dd3f
Hugues Bousquet <hbsqt@posteo.net>
2025-04-12 06:25:16 +0000

    Add modele and doc stg_hospital_dim

---
+++ models/staging/stg_hospitals_dim.sql
@@ -0,0 +1,7 @@
+select
+    row_number() over() as id_hospital,
+    r.Hospital as name_hospital
+from 
+    {{source("Raw_Anonymized", "Healthcare_Raw_Anonymized")}} r
+group by name_hospital
+order by id_hospital
